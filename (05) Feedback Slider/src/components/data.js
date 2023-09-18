3511da02f1952e7e9b1064fd8d599c2e6c7f7214
Ahmad Nematpour <93655950+AhmadNematpour@users.noreply.github.com>
2023-09-18 23:45:13 +0330

    Add files via upload

---
+++ (05) Feedback Slider/src/components/data.js
@@ -0,0 +1,29 @@
+const reviews = [
+    {
+        id: 1,
+        name: 'بیتا رحیمی',
+        job: 'طراح وب',
+        image: 'https://img.freepik.com/free-photo/friendly-smiling-woman-looking-pleased-front_176420-20779.jpg?w=1380&t=st=1678788274~exp=1678788874~hmac=9e598f75a4c0e217bc887737a65e846938e8ef836342427ea4a65b59d6c50c3e',
+        text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است',
+    
+    },
+    {
+        id: 2,
+        name: 'شایان شهینی',
+        job: 'گرافیست',
+        image: 'https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=1380&t=st=1678788278~exp=1678788878~hmac=b808ff0974171fb88a516670af624842a7bac72c84bcf13bc6667da2990d1b21',
+        text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است',
+    
+    },
+    {
+        id: 3,
+        name: 'رضا تاری',
+        job: 'برنامه نویس',
+        image: 'https://img.freepik.com/free-photo/good-mood-mature-man-burgundy-shirt-looking-positive_259150-56975.jpg?w=1380&t=st=1678788307~exp=1678788907~hmac=306ed3105fb2f2a0b312a3cd1723016bb2e5d9c7b6fc87a48e19ddb411a4c8e8',
+        text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. ت',
+    
+    }
+]
+
+
+export default reviews;
